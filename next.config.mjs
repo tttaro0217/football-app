82d4cf523fd98cbba88f5410973448c4aa88a7e9
tttaro0217 <rintaro.ishikawa@ysinc.co.jp>
2024-05-15 16:01:29 +0900

    update: リーグ一覧ページコーディング

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["crests.football-data.org"],
+  },
+};
 
 export default nextConfig;
